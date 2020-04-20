fe00c3ff5784a7a644c7afa83eb02c3f828b210e
andyleo97 <andyleo97@gmail.com>
2020-04-20 15:04:31 -0500

    issues with validation and added react material

--- src/Dialog/UserModals/CreateUserModal.js
+++ src/Dialog/UserModals/CreateUserModal.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react'
-import {Modal, Button, Form, Col, Row} from 'react-bootstrap'
+import { Form, Col, Row} from 'react-bootstrap'
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import './CreateUserModal.css';
-
+import { Dialog, DialogActions, DialogTitle, DialogContent, TextField, Button} from "@material-ui/core";
 
 class CreateUserModal extends Component {
     constructor(props, context) {
@@ -15,12 +15,11 @@ class CreateUserModal extends Component {
             email: '',
             password: '',
             confPassword: '',
-            buttonDisabled: true,
             errors: {
-                firstNameCheck: false,
-                lastNameCheck: false,
-                emailCheck: false,
-                passwordCheck: false
+                firstNameCheck: true,
+                lastNameCheck: true,
+                emailCheck: true,
+                passwordCheck: true
             }
         };
     }
@@ -30,108 +29,93 @@ class CreateUserModal extends Component {
             [e.target.name]: e.target.value
         });
         this.setState(this.validate());
+        console.log(this.state.errors.firstNameCheck)
     };
 
     validate = () => {
         // true means invalid, so our conditions got reversed
+        const expression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return { errors: {
                 firstNameCheck: this.state.firstName.length === 0,
                 lastNameCheck: this.state.lastName.length === 0,
-                emailCheck: this.state.email.length === 0,
-                passwordCheck: this.state.password === this.state.confPassword,
+                emailCheck: !(expression.test(String(this.state.email).toLowerCase())),
+                passwordCheck: !(this.state.password === this.state.confPassword),
             }
-        };
+            };
+
+    };
+
+    isValid = () => {
+        this.setState(this.validate());
+       return !this.state.errors.firstNameCheck && !this.state.errors.lastNameCheck && this.state.errors.emailCheck && this.state.errors.passwordCheck
     };
 
     onSubmit = e => {
+        //WHYYYYYYYYYYY
         e.preventDefault();
-        const user = {
-            firstName: this.state.firstName,
-            lastName: this.state.lastName,
-            email: this.state.email,
-            password: this.state.password,
-            confPassword: this.state.confPassword
-        };
+        console.log(!this.isValid())
+        if (!this.isValid){
+            console.log("Validation Error");
+
+        }
+        else {
+            console.log(!this.state.errors.firstNameCheck && !this.state.errors.lastNameCheck && this.state.errors.emailCheck && this.state.errors.passwordCheck)
+            console.log(this.state.errors);
+            const user = {
+                firstName: this.state.firstName,
+                lastName: this.state.lastName,
+                email: this.state.email,
+                password: this.state.password,
+                confPassword: this.state.confPassword
+            };
+
+            axios.post(`http://localhost:8080/createuser`, user);
+        }
 
 
-
-        axios.post(`http://localhost:8080/createuser`, user)
     };
 
 
     render() {
         const {onHide, show} = this.props;
         return (
-            <Modal
-                show={show}
-                size="lg"
-                aria-labelledby="contained-modal-title-vcenter"
-                centered>
-                <Modal.Header closeButton onClick={onHide}>
-                    <Modal.Title id="contained-modal-title-vcenter">
-                        Create a new User
-                    </Modal.Title>
-                </Modal.Header>
-                <Form>
-                    <Modal.Body>
-                        <p>
-                            <Form.Group as={Row}
-                                        controlId="main-login">
-                                <Form.Label column sm="2">
-                                    First Name
-                                </Form.Label>
-                                <Col sm="10">
-                                    <Form.Control className={this.state.errors.firstNameCheck ? "error" : ""} placeholder="First Name" name="firstName" value={this.state.firstName}
-                                                    onChange={this.onChange}/>
-                                </Col>
-                            </Form.Group>
-                            <Form.Group as={Row} controlId="exampleForm.ControlInput1">
-                                <Form.Label column sm="2">
-                                    Last Name
-                                </Form.Label>
-                                <Col sm="10">
-                                    <Form.Control className={this.state.errors.lastNameCheck ? "error" : ""} placeholder="Last Name" name="lastName" value={this.state.lastName}
-                                                  onChange={this.onChange}/>
-                                </Col>
-                            </Form.Group>
-                            <Form.Group as={Row} controlId="exampleForm.ControlInput1">
-                                <Form.Label column sm="2">
-                                    Email
-                                </Form.Label>
-                                <Col sm="10">
-                                    <Form.Control className={this.state.errors.emailCheck ? "error" : ""} placeholder="email@example.com" name="email" value={this.state.email}
-                                                  onChange={this.onChange}/>
-                                </Col>
-                            </Form.Group>
-
-                            <Form.Group as={Row} controlId="formPlaintextPassword">
-                                <Form.Label column sm="2">
-                                    Password
-                                </Form.Label>
-                                <Col sm="10">
-                                    <Form.Control className={this.state.errors.passwordCheck ? "error" : ""} type="password" placeholder="Password" name="password"
-                                                  value={this.state.password} onChange={this.onChange}/>
-                                </Col>
-                            </Form.Group>
-                            <Form.Group as={Row} controlId="formPlaintextPassword">
-                                <Form.Label column sm="2">
-                                    Confirm Password
-                                </Form.Label>
-                                <Col sm="10">
-                                    <Form.Control className={this.state.errors.passwordCheck ? "error" : ""} type="password" placeholder="Confirm Password" name="confPassword"
-                                                  value={this.state.confPassword} onChange={this.onChange}/>
-                                </Col>
-                            </Form.Group>
-                        </p>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="primary" onClick={this.onSubmit} disabled={this.state.buttonDisabled}>
+            <Dialog
+                maxWidth="xs"
+                fullWidth
+                open={show}
+                aria-labelledby="contained-modal-title-vcenter">
+                <DialogTitle id="form-dialog-title">Create an account</DialogTitle>
+                <DialogContent>
+                    <form autoComplete="off">
+                        <Row>
+                                <TextField error={this.state.errors.firstNameCheck} label="First Name" name="firstName"
+                                           onChange={this.onChange}/>
+                        </Row>
+                        <Row>
+                                <TextField  error={this.state.errors.lastNameCheck} required name="lastName" label="Last Name"
+                                           onChange={this.onChange}/>
+                        </Row>
+                        <Row>
+                                <TextField error={this.state.errors.emailCheck} required label="Email" placeholder="email@example.com" name="email"
+                                           onChange={this.onChange}/>
+                        </Row>
+                        <Row>
+                                <TextField error={this.state.errors.passwordCheck} required type="Password" label="Password" name="password"
+                                           onChange={this.onChange}/>
+                        </Row>
+                        <Row>
+                                <TextField error={this.state.errors.passwordCheck} required type="Password" label="Confirm Password" name="confPassword"
+                                           onChange={this.onChange}/>
+                        </Row>
+                    </form>
+                </DialogContent>
+                <DialogActions>
+                        <Button variant="contained" color="primary" onClick={this.onSubmit}>
                             Submit
                         </Button>
-                        <Button onClick={onHide}>Close</Button>
-                    </Modal.Footer>
-                </Form>
-            </Modal>
+                        <Button variant="contained" color="secondary" onClick={onHide} >Close</Button>
+                </DialogActions>
+            </Dialog>
         );
     }
 }
