2d1d6a592090ec9ce7a0d00253d4e75300f0d355
Dave Leo <daveleo@goofy.local>
2020-04-23 20:27:07 -0500

    fixed validation in create user

--- src/Dialog/UserModals/CreateUserModal.js
+++ src/Dialog/UserModals/CreateUserModal.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react'
-import { Form, Col, Row} from 'react-bootstrap'
+import {Row} from 'react-bootstrap'
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import './CreateUserModal.css';
-import { Dialog, DialogActions, DialogTitle, DialogContent, TextField, Button, Typography} from "@material-ui/core";
+import {Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField} from "@material-ui/core";
 
 class CreateUserModal extends Component {
+    MINIMUM_PASSWORD_LENGTH = 4
+
     constructor(props, context) {
         super(props, context);
 
@@ -15,73 +17,87 @@ class CreateUserModal extends Component {
             email: '',
             password: '',
             confPassword: '',
-            errors: {
-                firstNameCheck: true,
-                lastNameCheck: true,
-                emailCheck: true,
-                passwordCheck: true
-            }
+            firstNameCheck: false,
+            lastNameCheck: false,
+            emailCheck: false,
+            passwordCheck: false
         };
     }
 
+
     onChange = e => {
         this.setState({
-            [e.target.name]: e.target.value
+            [e.target.name]: e.target.value, errors: this.validate(e.target.name, e.target.value)
         });
-        this.setState(this.validate());
     };
 
-    validate = () => {
-        // true means invalid, so our conditions got reversed
+    validateFirstName = e => {
+        const val = e.target.value || ''
+        this.setState({firstName: val, firstNameCheck: val.length === 0 ? 'Cannot be empty' : ''})
+    }
+    validateLastName = e => {
+        const val = e.target.value || ''
+        this.setState({lastName: val, lastNameCheck: val.length === 0 ? 'Cannot be empty' : ''})
+    }
+    validateEmail = e => {
+        const val = e.target.value || ''
+        // eslint-disable-next-line no-useless-escape
         const expression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return { errors: {
-                firstNameCheck: this.state.firstName.length === 0,
-                lastNameCheck: this.state.lastName.length === 0,
-                emailCheck: !(expression.test(String(this.state.email).toLowerCase())),
-                passwordCheck: !((this.state.password === this.state.confPassword) && (this.state.password.length !== 0 && this.state.confPassword.length !== 0)),
-            }
-            };
 
-    };
+        this.setState({
+            email: val,
+            emailCheck: !(expression.test(String(val).toLowerCase())) ? 'Invalid email format' : ''
+        })
+    }
+
+    validatePassword = e => {
+        const {password, confPassword} = this.state
+        const cleanPassword = (e.target.name === 'password' ? e.target.value : password) || ''
+        const cleanConfPassword = (e.target.name === 'confPassword' ? e.target.value : confPassword) || ''
+        let errorMessage = cleanPassword !== cleanConfPassword ? 'Passwords don\'t match' : ''
+        if (!errorMessage) {
+            errorMessage = (cleanPassword.length < this.MINIMUM_PASSWORD_LENGTH) ? 'Password must have ' + this.MINIMUM_PASSWORD_LENGTH + ' characters' : ''
+        }
+        this.setState({
+            password: cleanPassword,
+            confPassword: cleanConfPassword,
+            passwordCheck: errorMessage,
+        })
+    }
 
     isValid = () => {
-        this.setState(this.validate());
-       return (!this.state.errors.firstNameCheck) && (!this.state.errors.lastNameCheck) && (!this.state.errors.emailCheck) && (!this.state.errors.passwordCheck)
+        const {
+            firstNameCheck, lastNameCheck, emailCheck, passwordCheck,
+            firstName, lastName, email, password, confPassword
+        } = this.state;
+        return (!firstNameCheck) && (!lastNameCheck) && (!emailCheck) && (!passwordCheck) && firstName &&
+            lastName && email && password && confPassword
     };
 
     onSubmit = e => {
         e.preventDefault();
-        console.log(!this.isValid())
-        if (!this.isValid()){
-            console.log("Validation Error");
+        const user = {
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            email: this.state.email,
+            password: this.state.password,
+            confPassword: this.state.confPassword
+        };
 
-        } else {
-            //console.log(!this.state.errors.firstNameCheck && !this.state.errors.lastNameCheck && this.state.errors.emailCheck && this.state.errors.passwordCheck)
-            console.log(this.state);
-            const user = {
-                firstName: this.state.firstName,
-                lastName: this.state.lastName,
-                email: this.state.email,
-                password: this.state.password,
-                confPassword: this.state.confPassword
+        axios.post(`http://localhost:8080/createuser`, user).then((response) => {
+            const newResponse = {
+                response
             };
-
-            axios.post(`http://localhost:8080/createuser`, user).then((response) => {
-                const newResponse = {
-                    response
-                };
-                this.setState(newResponse);
-            }, (error) => {
-                console.log(error);
-            });
-        }
-
-
+            this.setState(newResponse);
+        }, (error) => {
+            console.log(error);
+        });
     };
 
 
     render() {
         const {onHide, show} = this.props;
+        const {firstNameCheck, lastNameCheck, emailCheck, passwordCheck} = this.state;
         return (
             <Dialog
                 maxWidth="xs"
@@ -92,32 +108,38 @@ class CreateUserModal extends Component {
                 <DialogContent>
                     <form autoComplete="off">
                         <Row>
-                                <TextField error={this.state.errors.firstNameCheck} label="First Name" name="firstName"
-                                           onChange={this.onChange}/>
+                            <TextField error={!!firstNameCheck} helperText={firstNameCheck} label="First Name"
+                                       name="firstName"
+                                       onChange={this.validateFirstName}/>
                         </Row>
                         <Row>
-                                <TextField  error={this.state.errors.lastNameCheck} required name="lastName" label="Last Name"
-                                           onChange={this.onChange}/>
+                            <TextField error={!!lastNameCheck} helperText={lastNameCheck} required name="lastName"
+                                       label="Last Name"
+                                       onChange={this.validateLastName}/>
                         </Row>
                         <Row>
-                                <TextField error={this.state.errors.emailCheck} required label="Email" placeholder="email@example.com" name="email"
-                                           onChange={this.onChange}/>
+                            <TextField error={!!emailCheck} helperText={emailCheck} required label="Email"
+                                       placeholder="email@example.com"
+                                       name="email"
+                                       onChange={this.validateEmail}/>
                         </Row>
                         <Row>
-                                <TextField error={this.state.errors.passwordCheck} required type="Password" label="Password" name="password"
-                                           onChange={this.onChange}/>
+                            <TextField error={!!passwordCheck} required type="Password" label="Password" name="password"
+                                       onChange={this.validatePassword}/>
                         </Row>
                         <Row>
-                                <TextField  id="filled-error-helper-text" error={this.state.errors.passwordCheck} required helperText={this.state.errors.passwordCheck ? "Passwords Don't Match or Is Empty" : ""} type="Password" label="Confirm Password" name="confPassword"
-                                           onChange={this.onChange}/>
+                            <TextField id="filled-error-helper-text" error={!!passwordCheck} required
+                                       helperText={passwordCheck}
+                                       type="Password" label="Confirm Password" name="confPassword"
+                                       onChange={this.validatePassword}/>
                         </Row>
                     </form>
                 </DialogContent>
                 <DialogActions>
-                        <Button variant="contained" color="primary" onClick={this.onSubmit}>
-                            Submit
-                        </Button>
-                        <Button variant="contained" color="secondary" onClick={onHide} >Close</Button>
+                    <Button disabled={!this.isValid()} variant="contained" color="primary" onClick={this.onSubmit}>
+                        Submit
+                    </Button>
+                    <Button variant="contained" color="secondary" onClick={onHide}>Close</Button>
                 </DialogActions>
             </Dialog>
         );
@@ -126,7 +148,7 @@ class CreateUserModal extends Component {
 
 CreateUserModal.propTypes = {
     onHide: PropTypes.func,
-    show: PropTypes.func
+    show: PropTypes.bool
 };
 
 export default CreateUserModal;
